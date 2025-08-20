624eb2dfb41c03657d48ed13fe0e0326b9e8d827
احمد حسام محمد فرج <ahmdhsammfarge@gmail.com>
2025-08-20 23:52:15 +0300

    deploy I

--- next.config.js
+++ next.config.js
@@ -7,6 +7,11 @@ const nextConfig = {
         hostname: 'amplify-awsamplifygen2-a1-blogstoragebucket2d79d24-dl6ctrdxql1z.s3.eu-north-1.amazonaws.com',
         pathname: '/**',
       },
+      {
+        protocol: process.env.NEXT_PUBLIC_PROTOCOL,
+        hostname: process.env.NEXT_PUBLIC_DOMAIN,
+        pathname: '/**',
+      }
     ],
   },
 }
